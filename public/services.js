e696ac27d214d2593cd60eb68d46570ebb9da049
hshabbir <hshabbir@folio3.com>
2018-12-01 15:04:21 +0500

    1) added logo  2) removed unwanted code  3) code indentation 4) directive added for sidebar

--- public/services.js
+++ public/services.js
@@ -1,47 +1,45 @@
 angular.module('myApp.services', []).
-  factory('ergastAPIservice', function($http) {
+  factory('ergastAPIservice', function ($http) {
 
     var ergastAPI = {};
 
-       ergastAPI.registerUser=function(data)
-    {
-          var req = {
-             method: 'POST',
-             url: '/register',
-             data: data
-            };
-        $http(req);
+    ergastAPI.registerUser = function (data) {
+      var req = {
+        method: 'POST',
+        url: '/register',
+        data: data
+      };
+      $http(req);
 
     }
-    ergastAPI.login=function(abc)
-    {
-              var req = {
-                           method: 'POST',
-                           url: '/login',
-                           data: abc
-                        };
-              return $http(req); 
-    }
-    ergastAPI.getAd = function(id) {
-      console.log("yeh id hai!",id);
-                      return $http({
-                        method: 'GET', 
-                        url: '/ads/'+id
-                      });
-    }
-    ergastAPI.saveAd=function(abc)
-    {
-      var req={
-        method:'POST',
-        url:'/ad',
-        data:abc
+    ergastAPI.login = function (abc) {
+      var req = {
+        method: 'POST',
+        url: '/login',
+        data: abc
       };
       return $http(req);
     }
-   
-     ergastAPI.getAds = function() {
+    ergastAPI.getAd = function (id) {
+      console.log("yeh id hai!", id);
       return $http({
-        method: 'GET', 
+        method: 'GET',
+        url: '/ads/' + id
+      });
+    }
+    
+    ergastAPI.saveAd = function (abc) {
+      var req = {
+        method: 'POST',
+        url: '/ad',
+        data: abc
+      };
+      return $http(req);
+    }
+
+    ergastAPI.getAds = function () {
+      return $http({
+        method: 'GET',
         url: '/ads'
       });
     }
